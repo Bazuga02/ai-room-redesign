eeef0a244d3d3b19c66b5e98d88138310c8f2b6a
Bazuga02 <abhirai5515@gmail.com>
2025-01-14 15:44:57 +0530

    chore: Design adjustments

--- app/page.jsx
+++ app/page.jsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <div>
       <MainHeader />
-      <div className=" h-[80vh] flex flex-col justify-center items-center gap-5">
+      <div className=" h-[80vh] flex flex-col justify-center items-center gap-5 p-2">
         <div className=" flex flex-col items-center justify-center">
           <h1 className=" text-4xl font-bold text-blue-600">
             AI Room and Home{" "}
@@ -18,7 +18,7 @@ export default function Home() {
             Interior Redesign
           </h2>
         </div>
-        <p className=" text-lg font-semibold">
+        <p className=" text-lg font-semibold text-center">
           Transform your home with stunning AI-powered redesigns tailored to
           your unique style.{" "}
         </p>
@@ -33,12 +33,23 @@ export default function Home() {
           </Button>
         </Link>
 
-        <Image
-          src={"/hero-img.png"}
-          height={800}
-          width={800}
-          alt="hero image"
-        />
+        <div className=" flex w-[100px]  lg:w-full md:w-full items-center justify-center gap-8 ">
+          <Image
+            src={"/lib.jpg"}
+            height={450}
+            width={450}
+            alt="bad room image"
+            className=" shadow-2xl rounded-3xl"
+          />
+          <Image src={"/arrow1.jpg"} height={70} width={140} alt="arrows" />
+          <Image
+            src={"/Professional-Office.png"}
+            height={450}
+            width={450}
+            alt="office image"
+            className=" shadow-2xl rounded-3xl"
+          />
+        </div>
       </div>
       <Footer />
     </div>
